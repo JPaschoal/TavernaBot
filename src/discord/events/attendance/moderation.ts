fe7a25d214da3f5e43803e8970ebd970f4d87a9f
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2024-05-19 14:10:09 -0300

    adding response for user that send msg on chat

--- src/discord/events/attendance/moderation.ts
+++ src/discord/events/attendance/moderation.ts
@@ -1,4 +1,5 @@
 import { Event } from "#base";
+import { EmbedBuilder, TextChannel } from "discord.js";
 
 new Event({
     name: "Moderation",
@@ -6,6 +7,15 @@ new Event({
     run(message) {
       if(message.channel.id !== "1241637598271901778") return;
       if(!message.author.bot) {
+        const embed = new EmbedBuilder()
+        .setTitle(`${message.author.displayName} seu mula, você não pode falar aqui!`)
+        .setDescription("Se quiser saber quem são os turistas, use o comando `/turista` no chat de comandos.")
+        .setColor("#FF0000");
+
+        const channel = message.guild?.channels.cache.get("1241637598271901778") as TextChannel | undefined;
+
+        channel?.send({ embeds: [embed] });
+
         message.delete();
         return;
       }
