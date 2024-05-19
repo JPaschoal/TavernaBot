69445a14488f504c4af1103f1886198bc2b517fb
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2024-05-19 14:30:14 -0300

    Improve attendanceEvent to prevent duplicate messages for the same user in a day

--- src/discord/events/attendance/attendanceEvent.ts
+++ src/discord/events/attendance/attendanceEvent.ts
@@ -6,7 +6,7 @@ new Event({
     event: "voiceStateUpdate",
     async run(oldState, newState){
 
-      if (
+      if (  
         newState.channelId === null || // User left the channel
         newState.channelId === oldState.channelId || // Event triggered by other action than joining a channel
         oldState.channelId !== null || // User changed channel
@@ -17,12 +17,21 @@ new Event({
       const channelName = newState.channel?.name;
 
       const embed = new EmbedBuilder()
-        .setTitle("Marcando presença!")
-        .setDescription(`O mula ${user} se conectou no canal ${channelName}`)
+        .setTitle("Presente Professor!")
+        .setDescription(`O mula do ${user} se conectou no canal ${channelName}`)
         .setColor("#00FF00")
         .setTimestamp();
       
         const channel = oldState.guild?.channels.cache.get("1241637598271901778") as TextChannel | undefined;
+
+        // check if the user already has a message in the channel today
+        const messages = await channel?.messages.fetch();
+        const today = new Date().toLocaleDateString();
+        const todayMessages = messages?.filter((msg) => new Date(msg.createdTimestamp).toLocaleDateString() === today);
+        const userMessage = todayMessages?.find((msg) => msg?.embeds[0]?.description?.includes(user!));
+        
+        if(userMessage) return;
+
         channel?.send({ embeds: [embed] });
     }
   }
