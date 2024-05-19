0e55df912c484461e5217899210d3e2c122cdf70
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2024-05-19 12:27:29 -0300

    Update attendanceEvent to exclude bot users and improve embed color

--- src/discord/events/attendance/attendanceEvent.ts
+++ src/discord/events/attendance/attendanceEvent.ts
@@ -6,19 +6,20 @@ new Event({
     event: "voiceStateUpdate",
     async run(oldState, newState){
 
-      if(newState.channelId === null) return;
-      if(newState.channelId === oldState.channelId) return;
-      if(oldState.channelId !== null) return;
+      if (
+        newState.channelId === null || // User left the channel
+        newState.channelId === oldState.channelId || // Event triggered by other action than joining a channel
+        oldState.channelId !== null || // User changed channel
+        newState.member?.user.bot // User is a bot
+      ) return;
 
       const user = newState.member?.displayName;
       const channelName = newState.channel?.name;
 
-      if(newState.member?.user.bot) return;
-
       const embed = new EmbedBuilder()
         .setTitle("Marcando presença!")
         .setDescription(`O mula ${user} se conectou no canal ${channelName}`)
-        .setColor("#FF0000")
+        .setColor("#00FF00")
         .setTimestamp();
       
         const channel = oldState.guild?.channels.cache.get("1241637598271901778") as TextChannel | undefined;
