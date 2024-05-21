466d154fcda58096ee7d3bad4551ac5b98569477
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2024-05-20 22:41:16 -0300

    Remove unecessary return

--- src/discord/events/attendance/moderation.ts
+++ src/discord/events/attendance/moderation.ts
@@ -2,22 +2,21 @@ import { Event } from "#base";
 import { EmbedBuilder, TextChannel } from "discord.js";
 
 new Event({
-    name: "Moderation",
-    event: "messageCreate",
-    run(message) {
-      if(message.channel.id !== "1241637598271901778") return;
-      if(!message.author.bot) {
-        const embed = new EmbedBuilder()
+  name: "Moderation",
+  event: "messageCreate",
+  run(message) {
+    if (message.channel.id !== "1241637598271901778") return;
+    if (!message.author.bot) {
+      const embed = new EmbedBuilder()
         .setTitle(`${message.author.displayName} seu mula, você não pode falar aqui!`)
-        .setDescription("Se quiser saber quem são os turistas, use o comando `/turista` no chat de comandos.")
+        .setDescription("Se quiser saber quem são os turistas, use o comando `/turista` ou `/montartime` no chat.")
         .setColor("#FF0000");
 
-        const channel = message.guild?.channels.cache.get("1241637598271901778") as TextChannel | undefined;
+      const channel = message.guild?.channels.cache.get("1241637598271901778") as TextChannel | undefined;
 
-        channel?.send({ embeds: [embed] });
+      channel?.send({ embeds: [embed] });
 
-        message.delete();
-        return;
-      }
-    },
+      message.delete();
+    }
+  },
 });
