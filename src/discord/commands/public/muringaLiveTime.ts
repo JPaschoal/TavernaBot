ec6a45b93baf41aace2cf265d477f297ce380310
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2024-11-14 21:51:15 -0300

    Rename murilouptime command to streameruptime and add streamer option to check online duration

--- src/discord/commands/public/muringaLiveTime.ts
+++ src/discord/commands/public/muringaLiveTime.ts
@@ -1,19 +1,44 @@
 import { Command } from "#base";
 import { brBuilder, createEmbed } from "@magicyan/discord";
-import { ApplicationCommandType } from "discord.js";
+import { ApplicationCommandOptionType, ApplicationCommandType } from "discord.js";
 import settingsJson from "../../../../settings.json" with { type: "json" };
 import { getOAuthToken, getStream } from "#functions";
 
 new Command({
-  name: "murilouptime",
+  name: "streameruptime",
   description: "Verifica a quanto tempo o Muringa está online.",
   dmPermission: false,
+  options: [
+    {
+      name: "streamer",
+      description: "Streamer que deseja verificar o tempo online.",
+      type: ApplicationCommandOptionType.String,
+      required: true
+    }
+  ],
   type: ApplicationCommandType.ChatInput,
   async run(interaction) {
 
+    const { options } = interaction;
+
+    const streamer = options.getString("streamer", true);
+
     const token = await getOAuthToken();
 
-    const streams = await getStream(token);
+    const streams = await getStream(token, streamer);
+
+    if(streams.data.length === 0) {
+      const embed = createEmbed({
+        title: `${streamer} está offline.`,
+        description: brBuilder(
+          "• Não está transmitindo no momento."
+        ),
+        color: settingsJson.colors.danger
+      });
+
+      await interaction.reply({ embeds: [embed] });
+      return;
+    }
 
     // get Stream Up Time
     const stream = streams.data[0];
@@ -23,14 +48,14 @@ new Command({
     const diffHours = Math.floor(diff / 1000 / 60 / 60);
     const diffMinutes = Math.floor(diff / 1000 / 60 % 60);
 
-    const timeMessage = "• Murilo está online a " + diffHours + " horas e " + diffMinutes + " minutos.";
+    const timeMessage = `• ${streamer} está online a ` + diffHours + " horas e " + diffMinutes + " minutos.";
 
     const embed = createEmbed({
       title: "Live: ",
       description: brBuilder(
         `${timeMessage} \n`,
         `• Jogando: ${stream.game_name} \n`,
-        "• Link da live: [Clique aqui](https://twitch.tv/forgetfulxx)"
+        `• Link da live: [Clique aqui](https://twitch.tv/${streamer})`
 
       ),
       color: settingsJson.colors.info
