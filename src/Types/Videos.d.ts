6109527189df9bbe7516c7d33c39ff884e3ba8ff
Jhonatan Paschoal <jhonatanpaschoal@gmail.com>
2024-11-14 21:38:34 -0300

    Add TypeScript interfaces for Streams and Videos data structures

---
+++ src/Types/Videos.d.ts
@@ -0,0 +1,26 @@
+interface Videos {
+  data: Data[];
+  pagination: Pagination;
+}
+interface Pagination {
+  cursor: string;
+}
+interface Data {
+  id: string;
+  stream_id: string;
+  user_id: string;
+  user_login: string;
+  user_name: string;
+  title: string;
+  description: string;
+  created_at: string;
+  published_at: string;
+  url: string;
+  thumbnail_url: string;
+  viewable: string;
+  view_count: number;
+  language: string;
+  type: string;
+  duration: string;
+  muted_segments?: any;
+}
